ffeb5cde6609fdcfb7332587e1bd07b5109d2141
Lucas Cotta Muniz <lucascottamuniz@hotmail.com>
2023-05-17 12:17:21 -0300

    Adicionar arquivos de projeto.

---
+++ Imagem_Zoom/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Imagem_Zoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            trackBar1 = new TrackBar();
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(16, 13);
+            pictureBox1.MaximumSize = new Size(1920, 1080);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(437, 269);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 45);
+            button1.TabIndex = 3;
+            button1.Text = "Abrir Arquivo";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            trackBar1.Location = new Point(344, 18);
+            trackBar1.Maximum = 200;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(128, 45);
+            trackBar1.TabIndex = 4;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.AutoScroll = true;
+            panel1.BackColor = SystemColors.HotTrack;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(12, 69);
+            panel1.MaximumSize = new Size(1920, 1080);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(475, 314);
+            panel1.TabIndex = 5;
+            panel1.MouseWheel += MouseWheel;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Size:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 15);
+            label2.TabIndex = 7;
+            label2.Text = "X;Y";
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(12, 378);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(118, 27);
+            panel2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(385, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Zoom";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 413);
+            Controls.Add(label3);
+            Controls.Add(trackBar1);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Resize += Form1_Resize;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Button button1;
+        private TrackBar trackBar1;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Panel panel2;
+        private Label label3;
+    }
+}
---
+++ Imagem_Zoom/Form1.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Imagem_Zoom
+{
+    public partial class Form1 : Form
+    {
+        Image imgOriginal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.AutoScroll = true;
+            //pictureBox1.MouseWheel += pictureBox1_MouseWheel;
+            //pictureBox1.MouseWheel += pictureBox1_MouseMove;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog cleiton = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "JPEG|*.jpg" })
+            {
+                if (cleiton.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(cleiton.FileName);
+                    imgOriginal = pictureBox1.Image;
+                    Thread.Sleep(20);
+                    ResizeContainer();
+                    label2.Text = $"{pictureBox1.Width};{pictureBox1.Height} px";
+                }
+
+            }
+        }
+        Image? Zoom(Image img, Size size)
+        {
+            if(img != null)
+            {
+                Bitmap bmp = new Bitmap(img, img.Width + (img.Width * size.Width / 100), img.Height + (img.Height * size.Height / 100));
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                ResizeContainer();
+
+                return bmp;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        private void MouseWheel(object sender, MouseEventArgs e)
+        {
+            int delta = e.Delta / 12 * SystemInformation.MouseWheelScrollDelta / 120;
+
+            if ((trackBar1.Value + delta > trackBar1.Minimum) && (trackBar1.Value + delta < trackBar1.Maximum))
+            {
+                trackBar1.Value = trackBar1.Value + delta;
+
+                if (trackBar1.Value > 0)
+                {
+                    pictureBox1.Image = Zoom(imgOriginal, new Size(trackBar1.Value, trackBar1.Value));
+                }
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value > 0)
+            {
+                pictureBox1.Image = Zoom(imgOriginal, new Size(trackBar1.Value, trackBar1.Value));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Dispose();
+            }
+        }
+        private void ResizeContainer()
+        {
+            Control container = panel1;
+            Control viewPort = pictureBox1;
+
+            int x = container.Width / 2 - viewPort.Width / 2;
+            int y = container.Height / 2 - viewPort.Height / 2;
+
+            viewPort.Left = x; viewPort.Top = y;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ResizeContainer();
+        }
+
+
+    }
+}
