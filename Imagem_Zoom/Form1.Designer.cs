b9b2d51073ef3e20713b67385a795d245b6d3270
MattheusOliveiraBatista <matbatista.oliveira@gmail.com>
2023-05-22 11:03:52 -0300

    Versão refatorada

---
+++ Imagem_Zoom/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Imagem_Zoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            picImagemDaAnalise = new PictureBox();
+            btnAbrirProjeto = new Button();
+            imageList1 = new ImageList(components);
+            pnlPlanoDeFundoDaImagem = new Panel();
+            lblImagemComTamanhoOriginal = new Label();
+            pnlPlanoDeFundoParaDimensoesDaImagem = new Panel();
+            lblImagemComTamanhoEmTempoReal = new Label();
+            btnAtualizaXml = new Button();
+            trbZoomDaImagem = new TrackBar();
+            pnlPlanoDeFundoParaControles = new Panel();
+            lblZoom = new Label();
+            ofdAbrirImagem = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)picImagemDaAnalise).BeginInit();
+            pnlPlanoDeFundoDaImagem.SuspendLayout();
+            pnlPlanoDeFundoParaDimensoesDaImagem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trbZoomDaImagem).BeginInit();
+            pnlPlanoDeFundoParaControles.SuspendLayout();
+            SuspendLayout();
+            // 
+            // picImagemDaAnalise
+            // 
+            picImagemDaAnalise.Location = new Point(12, 3);
+            picImagemDaAnalise.Margin = new Padding(0, 80, 0, 0);
+            picImagemDaAnalise.Name = "picImagemDaAnalise";
+            picImagemDaAnalise.Size = new Size(458, 449);
+            picImagemDaAnalise.SizeMode = PictureBoxSizeMode.AutoSize;
+            picImagemDaAnalise.TabIndex = 1;
+            picImagemDaAnalise.TabStop = false;
+            // 
+            // btnAbrirProjeto
+            // 
+            btnAbrirProjeto.ImageAlign = ContentAlignment.MiddleLeft;
+            btnAbrirProjeto.ImageKey = "101671.png";
+            btnAbrirProjeto.ImageList = imageList1;
+            btnAbrirProjeto.Location = new Point(9, 19);
+            btnAbrirProjeto.Name = "btnAbrirProjeto";
+            btnAbrirProjeto.Size = new Size(81, 23);
+            btnAbrirProjeto.TabIndex = 3;
+            btnAbrirProjeto.Text = "Open File";
+            btnAbrirProjeto.TextAlign = ContentAlignment.MiddleRight;
+            btnAbrirProjeto.UseVisualStyleBackColor = true;
+            btnAbrirProjeto.Click += btnAbrirProjeto_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "Custom-Icon-Design-Mono-General-4-Refresh.512.png");
+            imageList1.Images.SetKeyName(1, "101671.png");
+            // 
+            // pnlPlanoDeFundoDaImagem
+            // 
+            pnlPlanoDeFundoDaImagem.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlPlanoDeFundoDaImagem.AutoScroll = true;
+            pnlPlanoDeFundoDaImagem.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            pnlPlanoDeFundoDaImagem.BackColor = Color.Transparent;
+            pnlPlanoDeFundoDaImagem.BorderStyle = BorderStyle.FixedSingle;
+            pnlPlanoDeFundoDaImagem.Controls.Add(picImagemDaAnalise);
+            pnlPlanoDeFundoDaImagem.Location = new Point(119, 12);
+            pnlPlanoDeFundoDaImagem.Name = "pnlPlanoDeFundoDaImagem";
+            pnlPlanoDeFundoDaImagem.Size = new Size(919, 473);
+            pnlPlanoDeFundoDaImagem.TabIndex = 5;
+            pnlPlanoDeFundoDaImagem.MouseWheel += MouseWheel;
+            // 
+            // lblImagemComTamanhoOriginal
+            // 
+            lblImagemComTamanhoOriginal.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lblImagemComTamanhoOriginal.AutoSize = true;
+            lblImagemComTamanhoOriginal.Location = new Point(39, 10);
+            lblImagemComTamanhoOriginal.Name = "lblImagemComTamanhoOriginal";
+            lblImagemComTamanhoOriginal.Size = new Size(143, 15);
+            lblImagemComTamanhoOriginal.TabIndex = 6;
+            lblImagemComTamanhoOriginal.Text = "Tamanho Original: X; Y px";
+            // 
+            // pnlPlanoDeFundoParaDimensoesDaImagem
+            // 
+            pnlPlanoDeFundoParaDimensoesDaImagem.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlPlanoDeFundoParaDimensoesDaImagem.Controls.Add(lblImagemComTamanhoEmTempoReal);
+            pnlPlanoDeFundoParaDimensoesDaImagem.Controls.Add(lblImagemComTamanhoOriginal);
+            pnlPlanoDeFundoParaDimensoesDaImagem.Location = new Point(12, 491);
+            pnlPlanoDeFundoParaDimensoesDaImagem.Name = "pnlPlanoDeFundoParaDimensoesDaImagem";
+            pnlPlanoDeFundoParaDimensoesDaImagem.Size = new Size(1025, 37);
+            pnlPlanoDeFundoParaDimensoesDaImagem.TabIndex = 8;
+            // 
+            // lblImagemComTamanhoEmTempoReal
+            // 
+            lblImagemComTamanhoEmTempoReal.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lblImagemComTamanhoEmTempoReal.AutoSize = true;
+            lblImagemComTamanhoEmTempoReal.Location = new Point(234, 10);
+            lblImagemComTamanhoEmTempoReal.Name = "lblImagemComTamanhoEmTempoReal";
+            lblImagemComTamanhoEmTempoReal.Size = new Size(251, 15);
+            lblImagemComTamanhoEmTempoReal.TabIndex = 9;
+            lblImagemComTamanhoEmTempoReal.Text = "Imagem com tamanho em tempo real: X; Y px\n";
+            // 
+            // btnAtualizaXml
+            // 
+            btnAtualizaXml.ImageKey = "Custom-Icon-Design-Mono-General-4-Refresh.512.png";
+            btnAtualizaXml.ImageList = imageList1;
+            btnAtualizaXml.Location = new Point(5, 120);
+            btnAtualizaXml.Name = "btnAtualizaXml";
+            btnAtualizaXml.Size = new Size(86, 23);
+            btnAtualizaXml.TabIndex = 9;
+            btnAtualizaXml.TextAlign = ContentAlignment.MiddleRight;
+            btnAtualizaXml.UseVisualStyleBackColor = true;
+            btnAtualizaXml.Click += btnAtualizaXml_Click;
+            // 
+            // trbZoomDaImagem
+            // 
+            trbZoomDaImagem.Location = new Point(3, 58);
+            trbZoomDaImagem.Maximum = 300;
+            trbZoomDaImagem.Name = "trbZoomDaImagem";
+            trbZoomDaImagem.Size = new Size(81, 45);
+            trbZoomDaImagem.TabIndex = 4;
+            trbZoomDaImagem.Value = 1;
+            trbZoomDaImagem.Visible = false;
+            trbZoomDaImagem.Scroll += trbZoomDaImagem_Scroll;
+            // 
+            // pnlPlanoDeFundoParaControles
+            // 
+            pnlPlanoDeFundoParaControles.Controls.Add(btnAbrirProjeto);
+            pnlPlanoDeFundoParaControles.Controls.Add(lblZoom);
+            pnlPlanoDeFundoParaControles.Controls.Add(btnAtualizaXml);
+            pnlPlanoDeFundoParaControles.Controls.Add(trbZoomDaImagem);
+            pnlPlanoDeFundoParaControles.Location = new Point(12, 12);
+            pnlPlanoDeFundoParaControles.Name = "pnlPlanoDeFundoParaControles";
+            pnlPlanoDeFundoParaControles.Size = new Size(101, 473);
+            pnlPlanoDeFundoParaControles.TabIndex = 2;
+            // 
+            // lblZoom
+            // 
+            lblZoom.AutoSize = true;
+            lblZoom.Location = new Point(22, 88);
+            lblZoom.Name = "lblZoom";
+            lblZoom.Size = new Size(39, 15);
+            lblZoom.TabIndex = 10;
+            lblZoom.Text = "Zoom";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1050, 539);
+            Controls.Add(pnlPlanoDeFundoDaImagem);
+            Controls.Add(pnlPlanoDeFundoParaDimensoesDaImagem);
+            Controls.Add(pnlPlanoDeFundoParaControles);
+            Name = "Form1";
+            Text = "Imagem Zoom";
+            FormClosing += Form1_FormClosing;
+            Resize += Form1_Resize;
+            ((System.ComponentModel.ISupportInitialize)picImagemDaAnalise).EndInit();
+            pnlPlanoDeFundoDaImagem.ResumeLayout(false);
+            pnlPlanoDeFundoDaImagem.PerformLayout();
+            pnlPlanoDeFundoParaDimensoesDaImagem.ResumeLayout(false);
+            pnlPlanoDeFundoParaDimensoesDaImagem.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trbZoomDaImagem).EndInit();
+            pnlPlanoDeFundoParaControles.ResumeLayout(false);
+            pnlPlanoDeFundoParaControles.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox picImagemDaAnalise;
+        private Button btnAbrirProjeto;
+        private Panel pnlPlanoDeFundoDaImagem;
+        private Label lblImagemComTamanhoOriginal;
+        private Panel pnlPlanoDeFundoParaDimensoesDaImagem;
+        private Label lblImagemComTamanhoEmTempoReal;
+        private Button btnAtualizaXml;
+        private TrackBar trbZoomDaImagem;
+        private Panel pnlPlanoDeFundoParaControles;
+        private Label lblZoom;
+        private OpenFileDialog ofdAbrirImagem;
+        private ImageList imageList1;
+    }
+}
---
+++ Imagem_Zoom/Form1.cs
@@ -0,0 +1,202 @@
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Imagem_Zoom
+{
+    public partial class Form1 : Form
+    {
+        Image imgOriginal;
+
+        private int larguraAposZoom;
+        private int alturaAposZoom;
+        private string imagemDaAnalise;
+        private string diretorioDoProjeto;
+
+        public int LarguraAposZoom
+        {
+            get { return larguraAposZoom; }
+            set { larguraAposZoom = value; }
+        }
+        public int AlturaAposZoom
+        {
+            get { return alturaAposZoom; }
+            set { alturaAposZoom = value; }
+        }
+        public string ImagemDaAnalise
+        {
+            get { return imagemDaAnalise; }
+            set { imagemDaAnalise = value; }
+        }
+        public string DiretorioDoProjeto
+        {
+            get { return diretorioDoProjeto; }
+            set { diretorioDoProjeto = value; }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pnlPlanoDeFundoDaImagem.AutoScroll = true;
+            trbZoomDaImagem.Visible = false;
+            lblZoom.Visible = false;
+            btnAtualizaXml.Visible = false;
+        }
+
+
+        private void RedimensionarConteudo()
+        {
+            Control recipiente = pnlPlanoDeFundoDaImagem;
+            Control janelaDeExibicao = picImagemDaAnalise;
+
+            janelaDeExibicao.Left = recipiente.Width / 2 - janelaDeExibicao.Width / 2; //= x;
+            janelaDeExibicao.Top = recipiente.Height / 2 - janelaDeExibicao.Height / 2;//= y;
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            RedimensionarConteudo();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (picImagemDaAnalise.Image != null)
+            {
+                picImagemDaAnalise.Dispose();
+            }
+        }
+
+        private void CriacaoDaPastaEXml(int largura, int altura, string imagemComSeuDiretorio)
+        {
+            string img = Path.GetFileNameWithoutExtension(imagemComSeuDiretorio);
+            string caminhoDoProjeto = $@"C:\{img}";
+            string nomeDaImagemComExtensao = Path.GetFileName(imagemComSeuDiretorio);
+
+            if (!Directory.Exists(caminhoDoProjeto))
+                Directory.CreateDirectory(caminhoDoProjeto);
+
+            System.IO.File.Copy(imagemComSeuDiretorio, caminhoDoProjeto + "\\" + nomeDaImagemComExtensao, true);
+
+            //Se o diretório existir, cria o arquivo xml
+            //if (Directory.Exists($@"C:\{somenteNomeDaImagem})
+
+            XmlTextWriter arquivoXml =
+                new XmlTextWriter($@"{caminhoDoProjeto + "\\" + img}.xml", System.Text.Encoding.UTF8);
+            arquivoXml.WriteStartDocument();
+            arquivoXml.Formatting = Formatting.Indented;
+            arquivoXml.WriteStartElement("Dados");
+            arquivoXml.WriteComment("");
+            arquivoXml.WriteElementString("NomeDaImagem", nomeDaImagemComExtensao);
+            arquivoXml.WriteElementString("LarguraDaImagem", largura.ToString());
+            arquivoXml.WriteElementString("AlturaDaImagem", altura.ToString());
+
+
+            arquivoXml.Close();
+            MessageBox.Show("Arquivo XML gerado com sucesso.", "Arquivo XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ImagemDaAnalise = img;
+            DiretorioDoProjeto = caminhoDoProjeto;
+        }
+
+        private void btnAbrirProjeto_Click(object sender, EventArgs e)
+        {
+
+            string nomeDaImagemComDiretorio = string.Empty;
+
+            ofdAbrirImagem.FileName = "";
+            ofdAbrirImagem.Multiselect = false;
+            ofdAbrirImagem.ValidateNames = true;
+            ofdAbrirImagem.Filter = "Arquivos de Imagem|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            ofdAbrirImagem.FilterIndex = 1;
+            ofdAbrirImagem.RestoreDirectory = true;
+
+            if (ofdAbrirImagem.ShowDialog() == DialogResult.OK)
+            {
+                nomeDaImagemComDiretorio = ofdAbrirImagem.FileName;
+                picImagemDaAnalise.Image = Image.FromFile(ofdAbrirImagem.FileName);
+                imgOriginal = picImagemDaAnalise.Image;
+                trbZoomDaImagem.Visible = true;
+                lblZoom.Visible = true;
+                btnAtualizaXml.Visible = true;
+                Thread.Sleep(20);
+                RedimensionarConteudo();
+
+                lblImagemComTamanhoOriginal.Text = $"Tamanho Original: {imgOriginal.Width}; {imgOriginal.Height} px";
+
+                CriacaoDaPastaEXml(picImagemDaAnalise.Width, picImagemDaAnalise.Height, nomeDaImagemComDiretorio);
+            }
+        }
+
+        private void btnAtualizaXml_Click(object sender, EventArgs e)
+        {
+            XElement escreveXml = new XElement("Atualização");
+
+            escreveXml.Add(new XElement("LarguraPosZoom", LarguraAposZoom.ToString()));
+            escreveXml.Add(new XElement("AlturaPosZoom", AlturaAposZoom.ToString()));
+
+            XElement xml = XElement.Load($@"{DiretorioDoProjeto + "\\" + ImagemDaAnalise}.xml");
+            xml.Add(escreveXml);
+            xml.Save($@"{DiretorioDoProjeto + "\\" + ImagemDaAnalise}.xml");
+            MessageBox.Show("Arquivo XML atualizado com sucesso.", "Arquivo XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void MouseWheel(object sender, MouseEventArgs e)
+        {
+            int delta = e.Delta / 12 * SystemInformation.MouseWheelScrollDelta / 120;
+
+            if ((trbZoomDaImagem.Value + delta >= trbZoomDaImagem.Minimum) && (trbZoomDaImagem.Value + delta <= trbZoomDaImagem.Maximum))
+            {
+
+                trbZoomDaImagem.Value = trbZoomDaImagem.Value + delta;
+
+                if (trbZoomDaImagem.Value >= 0)
+                {
+                    picImagemDaAnalise.Image = Zoom(imgOriginal, new Size(trbZoomDaImagem.Value, trbZoomDaImagem.Value));
+                }
+                else
+                {
+                    picImagemDaAnalise.Image = Zoom(imgOriginal, new Size(trbZoomDaImagem.Value, trbZoomDaImagem.Value));
+                }
+
+            }
+        }
+
+        private void trbZoomDaImagem_Scroll(object sender, EventArgs e)
+        {
+            if (trbZoomDaImagem.Value >= 0)
+                picImagemDaAnalise.Image = Zoom(imgOriginal, new Size(trbZoomDaImagem.Value, trbZoomDaImagem.Value));
+
+            else
+                picImagemDaAnalise.Image = Zoom(imgOriginal, new Size(trbZoomDaImagem.Value, trbZoomDaImagem.Value));
+        }
+
+        Image? Zoom(Image imagemDaAnaliseSemZoom, Size tamanho)
+        {
+            if (imagemDaAnaliseSemZoom != null)
+            {
+
+                Bitmap bmp = new Bitmap(imagemDaAnaliseSemZoom, imagemDaAnaliseSemZoom.Width + (imagemDaAnaliseSemZoom.Width * tamanho.Width / 100),
+                                        imagemDaAnaliseSemZoom.Height + (imagemDaAnaliseSemZoom.Height * tamanho.Height / 100));
+
+                lblImagemComTamanhoEmTempoReal.Text = $"Imagm com tamanho em tempo real: {bmp.Width};{bmp.Height} px";
+                Graphics g = Graphics.FromImage(bmp);
+
+                LarguraAposZoom = bmp.Width;
+                AlturaAposZoom = bmp.Height;
+
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                RedimensionarConteudo();
+
+                return bmp;
+            }
+            else
+                return null;
+        }
+
+    }
+}
+
+
